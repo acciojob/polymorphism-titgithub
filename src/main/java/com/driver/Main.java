62edd559272a4077446b2894557b2cb350b7b458
devashree <devashree2454@gmail.com>
2023-09-15 10:16:19 +0530

    poly changed done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[]args){
+        Product p = new Product();
+        int ans = p.product(10,20);
+        int ans1 = p.product(10,20,30);
+        double ans3 = p.product(1.0,2.0);
+        System.out.println(ans+" "+ans1+" "+ans3);
+    }
+    public static class Product {
+        public int product(int x, int y) {
 
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+
+            return x * y * z;
+        }
+
+        public double product(double x, double y)
+        {
+            return x * y;
+        }
+
+
+
+    }
 }
+
